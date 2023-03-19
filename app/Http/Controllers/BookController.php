2630d0a7243d04183e65da4ee94847dba414049f
nor1k0 <a8113215@gmail.com>
2023-03-19 13:06:26 +0000

    “コメント”

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -42,7 +42,7 @@ class BookController extends Controller
             's1_title' => 'required|max:100',
             's1_body' => 'required|max:200',
             's2_title' => 'required|max:100',
-            //  's2_body' => 'required|max:200',
+            's2_body' => 'required|max:200',
              's3_title' => 'required|max:100',
              's3_body' => 'required|max:200',
             // 's4_title' => 'max:100',
@@ -138,7 +138,7 @@ class BookController extends Controller
             's1_title' => 'required|max:255',
             's1_body' => 'required|max:255',
              's2_title' => 'required|max:100',
-            //  's2_body' => 'required|max:200',
+             's2_body' => 'required|max:200',
              's3_title' => 'required|max:100',
              's3_body' => 'required|max:200',
             'flag_open' => 'required'
